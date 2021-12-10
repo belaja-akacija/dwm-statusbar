3a17b10cc076d2b8aac9cb6ccae65ea47c1d32da
belaja-akacija <BA@pm.me>
2021-12-10 14:36:05 -0800

    fixed lagging bug

--- statusbar.sh
+++ statusbar.sh
@@ -6,13 +6,13 @@
 
 Main()
 {
-MUSIC=$(cmus-remote -Q | grep 'status' | cut -d ' ' -f 2-)
-SENSORS=$(sensors | grep 'Package id 0:' | awk '{print substr($4,2)}')
-MEM=$(free -h | awk '/^Mem/ { print $7"/"$2 }' | sed s/i//g)
-DISK=$(df -h /dev/sda3 | awk '/dev/ { print $4 }')
-BATTERY=$(bash battery-stat.sh)
-DATE=$(date '+%b %d (%a) %R ')
-UPTIME=$(bash uptime.sh)
+  MUSIC=$(cmus-remote -Q | grep 'status' | cut -d ' ' -f 2-)
+  SENSORS=$(sensors | grep 'Package id 0:' | awk '{print substr($4,2)}')
+  MEM=$(free -h | awk '/^Mem/ { print $7"/"$2 }' | sed s/i//g)
+  DISK=$(df -h /dev/sda3 | awk '/dev/ { print $4 }')
+  BATTERY=$(bash battery-stat.sh)
+  DATE=$(date '+%b %d (%a) %R ')
+  UPTIME=$(bash uptime.sh)
   while true;do
 
     if [ "$MUSIC" = "playing" ];
@@ -27,29 +27,36 @@ UPTIME=$(bash uptime.sh)
         i=0
         while (( $i <= ( ${#TITLE}/4 ) ));
         do
+          music_check=$(cmus-remote -Q | grep 'status' | cut -d ' ' -f 2-)
           #echo ${STRING:x:y}
+          if [ ! $music_check = "playing" ];
+          then
+            break
+          else
           xsetroot -name "${TITLE:x:y}... | $UPTIME | $SENSORS | Mem: $MEM | $DISK | $BATTERY | $DATE"
-          let x++
-          let y++
-          let i++
-          sleep 0.7
-          tput cuu1
-          tput el
+            let x++
+            let y++
+            let i++
+            sleep 0.7
+            tput cuu1
+            tput el
+          fi
         done
+
         Main
         #TITLE=$(echo $TITLE | cut -c1-30)...
         #echo "$TITLE"
       else
 
-          xsetroot -name "${TITLE} | $UPTIME | $SENSORS | Mem: $MEM | $DISK | $BATTERY | $DATE"
+        xsetroot -name "${TITLE} | $UPTIME | $SENSORS | Mem: $MEM | $DISK | $BATTERY | $DATE"
         sleep 0.07
         tput cuu1
         tput el
         Main
       fi
     else
-          xsetroot -name "$UPTIME | $SENSORS | Mem: $MEM | $DISK | $BATTERY | $DATE"
-          Main
+      xsetroot -name "$UPTIME | $SENSORS | Mem: $MEM | $DISK | $BATTERY | $DATE"
+      Main
     fi
   done
 }
