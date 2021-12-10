932630ee8cf7c773c9f862ae4379b0a5053416a0
belaja-akacija <BA@pm.me>
2021-12-10 13:56:00 -0800

    added basic scrolling to truncated text. Doesn't work with dwmblocks, though

--- playing.sh
+++ playing.sh
@@ -1,17 +1,48 @@
 #!/bin/bash
 
-MUSIC=$(cmus-remote -Q | grep 'status' | cut -d ' ' -f 2-)
+# Basic scrolling of truncated text
+# Cannot get this to work with dwmblocks without using xsetroot (neither printf nor echo refresh, although it works in the terminal)
 
-if [ "$MUSIC" = "playing" ];
-then
-  TITLE=$(cmus-remote -Q | grep 'file' | sed -e 's/file\s\/.\+\///g' -e 's/\..\+//g')
-  if [ ${#TITLE} -gt "30" ];
-  then
-    TITLE=$(echo $TITLE | cut -c1-30)...
-    echo "$TITLE"
-  else
-    echo "$TITLE"
-  fi
-else
-  exit 0
-fi
+MUSIC=$(cmus-remote -Q | grep 'status' | cut -d ' ' -f 2-)
+Main()
+{
+  while true;do
+
+    if [ "$MUSIC" = "playing" ];
+    then
+
+      TITLE=$(cmus-remote -Q | grep 'file' | sed -e 's/file\s\/.\+\///g' -e 's/\..\+//g')
+      if [ ${#TITLE} -gt "30" ];
+      then
+
+        x=0
+        y=8
+        i=0
+        while (( $i <= ( ${#TITLE}/3 ) ));
+        do
+          #echo ${STRING:x:y}
+          xsetroot -name "${TITLE:x:y}..."
+          let x++
+          let y++
+          let i++
+          sleep 0.7
+          tput cuu1
+          tput el
+        done
+        Main
+        #TITLE=$(echo $TITLE | cut -c1-30)...
+        #echo "$TITLE"
+      else
+
+        xsetroot -name "$TITLE"
+        sleep 0.07
+        tput cuu1
+        tput el
+      fi
+    else
+
+      exit 0
+    fi
+  done
+}
+Main
