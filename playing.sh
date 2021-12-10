0c1ff632aa0d62d2a54015fdfbc941f4ee0234e5
belaja-akacija <BA@pm.me>
2021-12-10 10:55:32 -0800

    got rid of unnecessary comments

--- playing.sh
+++ playing.sh
@@ -5,9 +5,6 @@ MUSIC=$(cmus-remote -Q | grep 'status' | cut -d ' ' -f 2-)
 if [ "$MUSIC" = "playing" ];
 then
   TITLE=$(cmus-remote -Q | grep 'file' | sed -e 's/file\s\/.\+\///g' -e 's/\..\+//g')
-  #LENGTH=${#TITLE}
-  #TRUNC=$(echo $TITLE | cut -c1-30)
-  #echo $LENGTH
   if [ ${#TITLE} -gt "30" ];
   then
     TITLE=$(echo $TITLE | cut -c1-30)...
