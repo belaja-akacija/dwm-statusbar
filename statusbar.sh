51801b2810bef7978b0844e38730c528fc101d78
belaja-akacija <BA@pm.me>
2021-12-10 14:26:42 -0800

    made a hasty statusbar that does the same thing as dwmblocks

---
+++ statusbar.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Basic scrolling of truncated text
+# Cannot get this to work with dwmblocks without using xsetroot (neither printf nor echo refresh, although it works in the terminal)
+
+
+Main()
+{
+MUSIC=$(cmus-remote -Q | grep 'status' | cut -d ' ' -f 2-)
+SENSORS=$(sensors | grep 'Package id 0:' | awk '{print substr($4,2)}')
+MEM=$(free -h | awk '/^Mem/ { print $7"/"$2 }' | sed s/i//g)
+DISK=$(df -h /dev/sda3 | awk '/dev/ { print $4 }')
+BATTERY=$(bash battery-stat.sh)
+DATE=$(date '+%b %d (%a) %R ')
+UPTIME=$(bash uptime.sh)
+  while true;do
+
+    if [ "$MUSIC" = "playing" ];
+    then
+
+      TITLE=$(cmus-remote -Q | grep 'file' | sed -e 's/file\s\/.\+\///g' -e 's/\..\+//g')
+      if [ ${#TITLE} -gt "30" ];
+      then
+
+        x=0
+        y=8
+        i=0
+        while (( $i <= ( ${#TITLE}/4 ) ));
+        do
+          #echo ${STRING:x:y}
+          xsetroot -name "${TITLE:x:y}... | $UPTIME | $SENSORS | Mem: $MEM | $DISK | $BATTERY | $DATE"
+          let x++
+          let y++
+          let i++
+          sleep 0.7
+          tput cuu1
+          tput el
+        done
+        Main
+        #TITLE=$(echo $TITLE | cut -c1-30)...
+        #echo "$TITLE"
+      else
+
+          xsetroot -name "${TITLE} | $UPTIME | $SENSORS | Mem: $MEM | $DISK | $BATTERY | $DATE"
+        sleep 0.07
+        tput cuu1
+        tput el
+        Main
+      fi
+    else
+          xsetroot -name "$UPTIME | $SENSORS | Mem: $MEM | $DISK | $BATTERY | $DATE"
+          Main
+    fi
+  done
+}
+Main
