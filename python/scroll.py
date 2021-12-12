2626c0a19a50c4a062db83741df586b04fdd5b80
belaja-akacija <BA@pm.me>
2021-12-12 13:55:25 -0800

    cleaned up and better comments

--- python/scroll.py
+++ python/scroll.py
@@ -5,19 +5,14 @@ import sys
 def main():
     r = 0
     x = 0
-    os.system("clear")
-    # stringlist = list(format(sys.stdin.read())) # Comment
-    # stringlist = list("String to scroll!   ") # Comment
-    stream = os.popen("cmus-remote -Q | grep 'file' | sed -e 's/file\s\/.\+\///g' -e 's/\..\+//g'") # Uncomment
-    stringlist = list(stream.read().strip() + "      ")# Uncomment
+    stream = os.popen("cmus-remote -Q | grep 'file' | sed -e 's/file\s\/.\+\///g' -e 's/\..\+//g'") # The command to run
+    stringlist = list(stream.read().strip() + "      ") # adjust whitespace in quotes
     while (r <= len(stringlist)):
         stringlist.append(stringlist[x])
-        xsetrooter = "".join(stringlist) + "..."
+        xsetrooter = "".join(stringlist) # adjust spacing in the quotes
         os.system("xsetroot"+" -name "+"'"+xsetrooter[:]+"'") # adjust the width of the textbox in [:]; ex. [:10] -- from index 0 to 10
-        # print("".join(stringlist), end='')
         stringlist.remove(stringlist[0])
         time.sleep(0.25)
-        # print('\r', end='')
         ++x
         ++r
 
